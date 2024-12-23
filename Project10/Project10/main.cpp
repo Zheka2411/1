9f8f662cad5657edd75e0e30baa1509e09ac8796
Zheka2411 <131461545+Zheka2411@users.noreply.github.com>
2024-12-23 14:06:45 +0000

    Add files via upload

---
+++ Project10/Project10/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+struct Car {
+    string name;
+    string model;
+    double price;
+    string country;
+};
+
+struct Rectangle {
+    double length;
+    double width;
+};
+
+int main() {
+    cout << "Hello, world!" << endl;
+    return 0;
+}
